c64ed062979a0cfff5f8a316598bd4cd376d667a
LPAVFN <lpavfn@gmail.com>
2022-03-29 17:09:48 +1100

    v 1.0

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ import selfcore from "selfcore";
 
 const client = new selfcore();
 const gateway = new selfcore.Gateway(
-  "OTU2NDExMTUxNTkwMjYwNzU3.YkD1lA._9NjhSARvsTDJ0pu7DOIm3sT9v4"
+  "TOKEN"
 );
 
 gateway.on("message", (m) => {
-  if (m.channel_id === "512010319795847182") {
+  if (m.channel_id === "CHANNELID") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
 
     client.sendWebhook(
-      "https://discord.com/api/webhooks/957899401026744372/HOKivNZYNFq544UAafaO7hzntb1k3xdizgJ6_C4tX3_zq5zrqzUoHv85RHtbUXhRSquw",
+      "WEBHOOKLINK",
       content
     );
   }
---
+++ dismir.bat
@@ -0,0 +1,5 @@
+:loop
+cd C:\Users\finadmin.FIN-SYD-LT26\Desktop\dismir
+node app.js
+timeout /t 7200
+goto loop
